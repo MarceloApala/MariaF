50e9939e1143bc86a2e3e2eb974ae129201cb4a9
MArcelo <cam5000810@est.univalle.edu>
2021-03-04 09:24:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        
+        <a href="https://clashroyale.com/es/ " t>
+         Deja el Class Maria
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Code.js
@@ -0,0 +1,29 @@
+//import React from 'react';
+
+
+function Code(params) {
+    return(
+        
+          <section className="section">
+            <div className="container">
+              <h1 className="title">
+                POR FAVOR MARIA
+              </h1>
+              <p className="subtitle">
+                Maria deja tu vicio de Class Royale hacelo por nosotros  <strong>Bulma</strong>!
+              </p>
+            </div>
+            <div className="field">
+             <label className="label">Name</label>
+             <div className="control">
+                <input class="input" type="text" placeholder="Text input"></input>
+            </div>
+            </div>
+          </section>
+         
+        
+        
+         
+    );
+}
+export default Code;
